cbb17a6bbe4c4319be897d15dcfbe16467ab84dd
Jack Li <ljh_app@qq.com>
2022-04-12 13:19:37 +0800

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -3,6 +3,7 @@
 
 # echo with source location. source this file or just use the alias
 
-alias echo='echo "$BASH_SOURCE":"$LINENO":"${FUNCNAME:-<FUNC>}": '
+shopt -s expand_aliases
+alias echo='echo $BASH_SOURCE:$LINENO:$FUNCNAME: '
 
 echo "echo with source location"

490d8bca673478f8a92938f82eb1c00e474a6a00
lijh8 <9011764+lijh8@users.noreply.github.com>
2025-03-11 15:44:48 +0800

    Add files via upload

---
+++ bash_tips.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# echo with source location
+shopt -s expand_aliases
+alias echo='echo "$BASH_SOURCE:$LINENO:$FUNCNAME:"'
+echo "hello bash"
+
+main()
+{
+    # source another script and call function in that script
+    . foo.sh
+    source foo.sh
+    foo
+
+    #---
+
+    # use local for variable inside function to avoid polluting caller scope;
+    # use local or declare with -a, -A for array inside function;
+    # use declare with -a, -A for array outside function;
+    # do not combine local and initialization: local name=abc; # no ;
+    # use local and assignment separately: local name; name=abc; # ok ;
+    # no space around assignment operator: name=abc;
+    local name
+    name=abc
+
+    #---
+
+    # quote variables or values for they could have spaces or special characters;
+    # variable parameter expansion $ , backtick ` , escape \ , work in double quoting;
+    local foo bar
+    foo='abc 123'
+    foo="abc 123"
+    bar="$foo"
+    bar="`date`"
+    bar="dollar: \$10"
+    echo "$bar"
+
+    #---
+
+    # no space around redirection operator 2>
+    local file
+    # file=$0
+    file="non-existent-test-file.txt"
+    # ls "$file"               && echo "$file exists" || echo "$file not exist"
+    ls "$file" 2>/dev/null     && echo "$file exists" || echo "$file not exist"
+    ls "$file" >/dev/null 2>&1 && echo "$file exists" || echo "$file not exist"
+
+    #---
+
+    # { list; }
+    # list of group command must be terminated with newline or semicolon;
+    # spaces needed around list to separate it from braces;
+
+    { echo; }
+
+    {
+        echo
+    }
+
+    #---
+
+    # use test command for comparison of conditional expression;
+    # use expr command for integer arithmetic, with exit status 2, 3 for error;
+    # do not use bc, awk, (( for arithmetic;
+    #  - for bc does not indicate error with exit status;
+    #  - for awk does not even show message on error;
+    #  - for (( aborts on error;
+    local a b c err
+    a=10; b=20;
+    # a=10; b=-10;
+    # a=10; b=3.14;
+    # a=10; b=abc20;
+    c=`expr $a + $b 2>/dev/null`
+    err=$?
+    test $err -eq 2 -o $err -eq 3 && echo "error:$err: $a + $b" || echo "ok:$err: $c"
+
+}
+
+main

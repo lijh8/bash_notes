d701941d783e13b9b8f7cc9519459ec130bd359c
lijh8 <9011764+lijh8@users.noreply.github.com>
2025-03-28 00:07:28 +0800

    Add files via upload

--- bash_tips.sh
+++ bash_tips.sh
@@ -20,11 +20,15 @@ echo3(){
 }
 
 echo4(){
-    echo "`awk '{print $2 ":" $1}' <<< \`caller\` `: $*"
+    echo "`caller | sed -E 's/([^ ]+) +([^ ]+)/\2:\1/'`: $*"
+}
+
+echo5(){
+    echo "`caller | awk '{print $2 ":" $1}'`: $*"
 }
 
 # bug: this shows main as FUNCNAME of the top level code outside any function
-# echo5(){
+# echo6(){
 #     echo "${BASH_SOURCE[1]}:${BASH_LINENO[0]}:${FUNCNAME[1]}: $*"
 # }
 
